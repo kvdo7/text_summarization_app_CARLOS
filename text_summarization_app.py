27440496a0358755885bcf110d566dfb12395dba
kvdo7 <166592457+kvdo7@users.noreply.github.com>
2024-04-18 06:02:27 -0600

    text_summarization_app.py

--- text_summarization_app.py
+++ text_summarization_app.py
@@ -9,7 +9,7 @@ from langchain.chains.summarize import load_summarize_chain
 st.set_page_config(page_title='🦜🔗 Text Summarization App')
 
 def generate_response(txt):
-    openai_api_key = os.getenv('OPENAI_API_KEY')
+    openai_api_key = os.getenv(sk-proj-ESytyXnCgpz4OeAMUCifT3BlbkFJIlbk8hPFbGNRQkXPOPXI)
     if openai_api_key is None:
         st.error("OpenAI API key is not set. Please check your environment variables.")
         return "API Key not available. Check environment settings."
