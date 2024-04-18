c50dccfb4c8042cc22b92d4f23aaaf29d16ca317
kvdo7 <166592457+kvdo7@users.noreply.github.com>
2024-04-18 06:12:53 -0600

    text_summarization_app.py

--- text_summarization_app.py
+++ text_summarization_app.py
@@ -8,11 +8,10 @@ from langchain.chains.summarize import load_summarize_chain
 # Configuración de la página, esto debe ser lo primero
 st.set_page_config(page_title='🦜🔗 Text Summarization App')
 
-def generate_response(txt):
-    openai_api_key = os.getenv(sk-proj-ESytyXnCgpz4OeAMUCifT3BlbkFJIlbk8hPFbGNRQkXPOPXI)
+def generate_response(txt, openai_api_key):
     if openai_api_key is None:
-        st.error("OpenAI API key is not set. Please check your environment variables.")
-        return "API Key not available. Check environment settings."
+        st.error("OpenAI API key is not set. Please enter your API key to use the app.")
+        return "API Key not available. Please enter your API key."
 
     llm = OpenAI(temperature=0, openai_api_key=openai_api_key)
     text_splitter = CharacterTextSplitter()
@@ -23,8 +22,9 @@ def generate_response(txt):
 
 st.title('🦜🔗 Text Summarization App')
 txt_input = st.text_area('Enter your text', '', height=200)
+openai_api_key = st.text_input('Enter your OpenAI API key', type='password')
 
 if st.button('Summarize'):
-    response = generate_response(txt_input)
+    response = generate_response(txt_input, openai_api_key)
     if response:
         st.write(response)
